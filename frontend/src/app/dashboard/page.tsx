69a1caaadfd2af45e574bf63717892ba11a99588
Giovanni Vaina <vainagiovanni96@gmail.com>
2025-02-01 21:54:30 +0100

    fix(pages): height from vh to dvh

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -6,7 +6,7 @@ import HeaderAlpaca from "@/app/ui/header-alpaca";
  */
 export default function Dashboard() {
     return (
-        <main className="flex flex-col h-screen w-screen max-w-screen-sm text-black bg-pink-300">
+        <main className="flex flex-col h-dvh w-dvw max-w-screen-sm text-black bg-pink-300">
             <HeaderAlpaca />
             <div className="flex-[5] bg-white"></div>
             <div className="flex-1 bg-pink-500"></div>
